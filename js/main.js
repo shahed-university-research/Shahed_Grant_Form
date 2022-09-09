9713ffbef29fb17c42502d7dd8adaef7246feaa8
shahed-university-research <113122053+shahed-university-research@users.noreply.github.com>
2022-09-09 17:10:13 +0430

    Update main.js

--- js/main.js
+++ js/main.js
@@ -277,7 +277,7 @@ function print_letter() {
     height: 1024,
     colorDark: "#000000",
     colorLight: "#ffffff",
-    correctLevel: QRCode.CorrectLevel.M
+    correctLevel: QRCode.CorrectLevel.L
   });
 
   qrcode.makeCode(qrcode_text);
